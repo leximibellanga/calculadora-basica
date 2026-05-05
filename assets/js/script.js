47e31e6a1ae4ac9b8de3e3b9eb35a6d71215ef6c
Leximíbel Langa <langaleximibel@gmail.com>
2026-05-05 23:45:25 +0200

    add functions

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+/* ============= Variaveis ================ */
+let res = 0
+let result = document.getElementById('result')
+const btnClear = document.getElementById('btn-clear')
+const btnPrev = document.getElementById('btn-prev')
+
+const numbers = document.getElementsByClassName('numero')
+const operadores = document.getElementsByClassName('operador')
+
+
+/* =========== Escrever na tela =========== */
+// numeros
+for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+
+    number.addEventListener('click', () => {
+        if (result.innerText != 0) 
+            result.textContent += number.value;
+        else 
+            result.textContent = number.value;
+    })
+}
+
+// operador
+for (let i = 0; i < operadores.length; i++) {
+    const operador = operadores[i];
+
+    operador.addEventListener('click', () => {
+        if (result.innerText.charAt(result.innerText.length - 1) !== "÷" && 
+            result.innerText.charAt(result.innerText.length - 1) !== "+" && 
+            result.innerText.charAt(result.innerText.length - 1) !== "-" &&
+            result.innerText.charAt(result.innerText.length - 1) !== "x")
+            result.textContent += operador.value
+    }) 
+}
+
+
+/* ========= Excluir ultimo digito ======= */
+const clearLast = () => {
+    if (result.innerText.length > 1)
+        result.textContent = result.innerText.substring(0, (result.innerText.length - 1))
+    else 
+        result.textContent = 0
+}
+btnPrev.addEventListener('click', clearLast)
+
+
+/* =========== Limpar Resultado =========== */
+const clear = () => {
+    result.textContent = "0"
+}
+btnClear.addEventListener('click', clear)
+
+
+
+
+/* =========== Calculos =========== */
+
+
+
